55353ef204d8d1e6c75650f248ef67b85b07ba73
VladL3301 <115415496+VladL3301@users.noreply.github.com>
2022-10-09 19:02:44 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,48 @@
+//задача 4
+#include <iostream>
+using namespace std;
+int main() {
+setlocale(LC_ALL, "Russian");
+//задача 4
+	//double y, x;
+	//cin >> x;
+	//if (x > 0) {
+		//y = sqrt (x - 2);
+	//}
+	//if (x = 0) {
+		//y = 0;
+	//}
+	//if (x < 0) {
+		//y = abs (0);
+	//}
+	//cout << y << endl;
+	//1 
+	//int hp;
+	//cin >> hp;
+	//bool r;
+	//r = hp > 0;
+	//cout << boolalpha << r;
+ //2 
+	//int c;
+	//cin >> c;
+	//if (c > 20) {
+		//cout << "on";
+	//}
+	//else {
+		//cout << "off";
+	//}
+	//5 
+ int pal;
+ cin >> pal;
+	 switch (pal) {
+	 case 1:cout << "большой" << endl; break;
+	 case 2:cout << "указательный" << endl; break;
+	 case 3:cout << "средний" << endl; break;
+	 case 4:cout << "безымянный" << endl; break;
+	 case 5:cout << "мизинец" << endl; break;
+	 default: cout << "ошибка";
+	 } 
+ }
+  
+
+	
